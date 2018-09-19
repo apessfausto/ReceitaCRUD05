faa1459c20c2e708c9ea4e498ce1ff355717380f
Fausto Caires Goncalves <psicotattoo@hotmail.com>
2018-09-18 23:57:36 -0300

    Criado os componetes da View.

---
+++ CRUD05/View/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using CRUD05.ViewModel;
+using System.Windows;
+
+namespace CRUD05
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DataContext = new ReceitaViewModel();
+        }
+    }
+}
