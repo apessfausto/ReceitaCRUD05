336f310c2dc95670a84c6aeecd1c25f47aed69f3
Fausto Caires Goncalves <psicotattoo@hotmail.com>
2018-09-18 23:58:24 -0300

    Criado os componentes do projeto.

---
+++ CRUD05/Model/Receita.cs
@@ -0,0 +1,11 @@
+﻿namespace CRUD05.Model
+{
+    public  class Receita 
+    {
+        public int Id { get; set; }
+        public string NomeReceita { get; set; }
+        public string Estilo { get; set; }
+        public string Ingredientes{ get; set; }
+
+    }
+}
